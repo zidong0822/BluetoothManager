8d8f94ab35cc6f19c8de72be99cc6ab1dc4b8661
HE Hongwei <hehongwei@microduino.cc>
2018-11-15 12:04:35 +0800

    init

---
+++ BluetoothManager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BluetoothManager
+//
+//  Created by Harvey He on 2018/11/15.
+//  Copyright © 2018 Harvey He. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
